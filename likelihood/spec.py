e7af92f97ce654409b361fbd6402769629cf120d
Guadalupe Canas Herrera <canasherrera@lievens.lorentz.leidenuniv.nl>
2020-02-19 11:54:54 +0100

    Updated and merged team1 and team2 tasks

--- likelihood/spec.py
+++ likelihood/spec.py
@@ -1,4 +1,10 @@
-#General imports
+""" 
+module: spec 
+
+Prototype computation of spectroscopic galaxy clustering likelihood. 
+"""
+
+#Global
 import numpy as np
 
 #Import auxilary classes
@@ -17,6 +23,31 @@ class Spec:
         """
         self.theory=theory
 
+    def beta_eqs(self):
+        """     
+        Computation of Eqns 25 - 27 of Euclid IST:L documentation, 
+        found below:     
+        https://www.overleaf.com/read/pvfkzvvkymbj     
+        """
+        # SJ: Set up power spectrum [note weirdly Cobaya has it as P(z,k) 
+        # SJ: instead of the more common P(k,z)]     
+        Pk_interpolator = self.theory.get_Pk_interpolator()     
+        self.Pk_delta = Pk_interpolator["delta_tot_delta_tot"]         
+        # SJ: For now, fix redshift (and scale), 
+        # fix galaxy bias = 1, and fix sigma8(z) = 1     
+        # Cobaya does not seem to allow for either growth rate alone or sigma8 alone 
+        # to be called yet (only their combination).     
+        zk = 0.5     
+        b_gal = 1.0     
+        sigma8 = 1.0     
+        # compute Eqns 25-27        
+        beta = theory.get_fsigma8(zk)/sigma8/b_gal     
+        Pk_gal = (b_gal**2.0)*Pk_delta.P(zk,0.02)     
+        P0k = (1.0 + 2.0/3.0*beta + 1.0/5.0*beta**2.0)*Pk_gal     
+        P2k = (4.0/3.0*beta + 4.0/7.0*beta**2.0)*Pk_gal    
+        P4k = (8.0/35.0*beta**2.0)*Pk_gal
+
+        # (GCH): maybe save as attributes P0k, P2k, P4k?
 
     def loglike(self):
         """
@@ -24,8 +55,10 @@ class Spec:
         -------
         loglike: float
                 loglike for GC spectroscopy observable
+                ln(likelihood) = -1/2 chi**2
         """
-        loglike=0.0
-        return loglike
+        # SJ: This will be the log-likelihood; 
+        # for now just return P(z,k) for fixed z and k.     
+        return self.Pk_delta.P(zk, 0.02)
 
 
