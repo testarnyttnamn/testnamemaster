4a2e7d60e422c0e647945c8dc3b04a00ddd1732d
Guadalupe Canas Herrera <canasherrera@lorentz.leidenuniv.nl>
2020-05-18 08:28:37 +0100

    Added error msg for bias test in test_shear.py

--- likelihood/tests/test_shear.py
+++ likelihood/tests/test_shear.py
@@ -92,7 +92,8 @@ class cosmoinitTestCase(TestCase):
 
     def test_phot_galbias(self):
         npt.assert_almost_equal(self.shear.phot_galbias(0.1, 0.3),
-                                self.phot_galbias_check)
+                                self.phot_galbias_check,
+                                err_msg='Error in photometric galaxy bias')
 
     def test_w_integrand(self):
         int_comp = self.shear.w_gamma_integrand(0.1, 0.2, self.flatnz)
