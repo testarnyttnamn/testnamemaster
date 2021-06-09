e6569c3fac325f18f80aa77b2b5284bdb17fdb76
canasherrera <canasherrera@lorentz.leidenuniv.nl>
2021-06-09 15:12:26 +0200

    comments addressed

--- likelihood/auxiliary/model_generator.py
+++ likelihood/auxiliary/model_generator.py
@@ -12,7 +12,7 @@ def model_generator(model=1):
     """
     Model generator function.
 
-    THIS IS A TENTATIVE TEST!
+    THIS IS A PROOF OF PRINCIPLE
 
     Parameters
     ----------
@@ -20,24 +20,43 @@ def model_generator(model=1):
         Tentative. Select number corresponding to a model.
     """
 
+    parent_path = str(
+            Path(
+                Path(__file__).resolve().parents[1]))
+
     # Right now the likelihood expects always the following params
-    nuisance_params = {
+    likelihood_params = {
         'like_selection': 1,
         'full_photo': True,
-        'NL_flag': False,
-        'aia': 1.72,
-        'nia': -0.41,
-        'bia': 0.0}
+        'NL_flag': False}
 
+    if model == 0:
+        pass
     if model == 1:
-        # If model = 1 is selected, bias are added
-        nuisance = str(
-            Path(
-                Path(__file__).resolve().parents[1])) \
-            + '/Models/nuisance_bias.yaml'
-        with open(nuisance) as file:
-            model_file = yaml.load(file, Loader=yaml.FullLoader)
-            nuisance_params.update(model_file)
-    params = str(Path(Path(__file__).resolve().parents[1])) + '/params.yaml'
-    with open(params, 'w') as outfile:
-        yaml.dump(nuisance_params, outfile, default_flow_style=False)
+        # If model = 1 is selected, bias and ia params are added
+        nuisance_bias_path = parent_path + '/Models/nuisance_bias.yaml'
+        nuisance_ia_path = parent_path + '/Models/nuisance_ia.yaml'
+        try:
+            with open(nuisance_bias_path) as file:
+                nuisance_bias_file = yaml.load(file, Loader=yaml.FullLoader)
+                likelihood_params.update(nuisance_bias_file)
+        except OSError as err:
+            print("Cannot open {0}. Error: {1}".format(nuisance_bias_path,
+                                                       err))
+        try:
+            with open(nuisance_ia_path) as file:
+                nuisance_ia_file = yaml.load(file, Loader=yaml.FullLoader)
+                likelihood_params.update(nuisance_ia_file)
+        except OSError as err:
+            print("Cannot open {0}. Error: {1}".format(nuisance_ia_path,
+                                                       err))
+    else:
+        print("ATTENTION: No other model is available. Please choose 1.")
+
+    # added warning
+    params_path = parent_path + '/params.yaml'
+    print('WARNING:\n')
+    print("Be aware that {} will be created or overwritten".format(
+        params_path))
+    with open(params_path, 'w') as outfile:
+        yaml.dump(likelihood_params, outfile, default_flow_style=False)
