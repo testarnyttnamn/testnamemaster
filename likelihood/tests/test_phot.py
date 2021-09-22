586ee49d5c99db0fe179441665fc6afa1ef2474b
Andrea Pezzotta <pezzotta@op09lm.local>
2021-09-22 14:05:22 +0200

    Fixed profiling

--- likelihood/tests/test_phot.py
+++ likelihood/tests/test_phot.py
@@ -125,10 +125,11 @@ class photoinitTestCase(TestCase):
         # mock_cosmo_dic['nuisance_parameters']['bia'] = 0
         # mock_cosmo_dic['nuisance_parameters']['nia'] = 0
 
-        p_matter = interpolate.RectBivariateSpline(zs_base,
-                                                   ks_base,
-                                                   pdd)
-        mock_cosmo_dic['Pmm_phot'] = p_matter
+
+        mock_cosmo_dic['Pmm_phot'] = \
+            interpolate.RectBivariateSpline(zs_base,
+                                            ks_base,
+                                            pdd)
         mock_cosmo_dic['Pgg_phot'] = \
             interpolate.RectBivariateSpline(zs_base,
                                             ks_base,
