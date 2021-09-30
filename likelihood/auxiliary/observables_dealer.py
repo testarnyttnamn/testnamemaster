7a57539be89c5d94956440eb4df12cf09c850524
canasherrera <canasherrera@lorentz.leidenuniv.nl>
2021-09-30 22:45:42 +0200

    forgot the main file...

---
+++ likelihood/auxiliary/observables_dealer.py
@@ -0,0 +1,65 @@
+"""observables_dealer
+
+Contains function to read the observable dictionary
+and plot the visualization of the matrix
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
+def observables_visualization(observables_dict, palette='standard'):
+    """
+    Observables visualization
+
+    Takes the observables dictionary read by Cobaya
+    and shows the visualization of the matrix
+
+    Parameters
+    ----------
+    observables_dict: dict
+        dictionary with the observables selection
+    palette: string
+        name of the visualiation color palette
+        Choose between 'standard', 'protanopia',
+        'deuteranopia'
+    """
+    # Checks if everything looks nice
+    observables_dict_checked = observables_selection_checker(observables_dict)
+
+    observables_df = pd.DataFrame(
+        observables_dict_checked).fillna(-1).astype(int).T
+
+    cmaps = {
+        'standard': ('white', '#f4d4d4', '#85c0f9'),
+        'protanopia': ('white', '#ae9c45', '#a7b8f8'),
+        'deuteranopia': ('white', '#c59434', '#a3b7f9')
+    }
+
+    sns.heatmap(observables_df, annot=False,
+                cmap=ListedColormap(cmaps[palette]), cbar=False)
+    plt.title("matrix with observables selection")
+    plt.show()
+    return observables_df
+
+
+def observables_selection_checker(observables_dict):
+    """
+    Observables selection checker
+
+    Checks and notifies the user if a selection is
+    not correct
+
+    Parameters
+    ----------
+    observables_dict: dict
+        dictionary with the observables selection
+    """
+    if observables_dict['WL']['GCspectro']:
+        observables_dict['WL']['GCspectro'] = False
+        print('ATTENTION: CLOE does not compute WLxGCspectro.\n')
+        print('It has been changed to "False". \n')
+    return observables_dict
