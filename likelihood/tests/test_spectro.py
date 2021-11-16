2131036d7c1c82a33de96381f5871d0678694870
Stefano Davini <davini@Stefanos-MacBook-Pro.local>
2021-11-16 16:32:49 +0100

    replaced NL_flag

--- likelihood/tests/test_spectro.py
+++ likelihood/tests/test_spectro.py
@@ -114,8 +114,8 @@ class specinitTestCase(TestCase):
                           'z_win': zs_base,
                           'k_win': ks_base,
                           'MG_sigma': MG_interp, 'c': const.c.to('km/s').value,
+                          'NL_flag': 1,
                           'nuisance_parameters': {
-                              'NL_flag': 1,
                               'b1_photo': 1.0997727037892875,
                               'b2_photo': 1.220245876862528,
                               'b3_photo': 1.2723993083933989,
@@ -205,7 +205,6 @@ class specinitTestCase(TestCase):
                         'k_win': ks_base,
                         'MG_sigma': MG_interp,
                         'nuisance_parameters': {
-                            'NL_flag': 1,
                             'b1_photo': 1.0997727037892875,
                             'b2_photo': 1.220245876862528,
                             'b3_photo': 1.2723993083933989,
