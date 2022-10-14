d765c2514d90da53451d77808243ef5b7f757762
Guadalupe Canas Herrera <canasherrera@lorentz.leidenuniv.nl>
2022-10-14 08:04:03 +0000

    WIP: fixed extrapolation k_min in the matter power spectrum

--- cloe/tests/test_wrapper.py
+++ cloe/tests/test_wrapper.py
@@ -24,8 +24,7 @@ class CobayaModel:
         # Note that this k_min does not currently interface with Cobaya
         # Once the Cobaya interface is adjusted to use a set k_min, the same
         # value should be used here.
-        self.k_min_Boltzmannn = 0.001
-        self.k_max_Boltzmannn = 10.0
+        self.k_max_Boltzmann = 10.0
         self.k_min_GC_phot_interp = 0.001
         self.k_max_GC_phot_interp = 50.0
         self.k_samp_GC = 100
